53619c645ecf35531c7d15d8587d35558bb1868a
m1dunfee <matthewgoldshire@gmail.com>
2025-01-05 19:24:52 -0600

    added react-router-dom. Need to fix the padding next and setup another page. check css etc.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import Header from './components/Header';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render(){
     return (
-      <div className='cd-flex background-section' >
+      //FIXME padding under the header isn't right
+      <div className='cd-flex background-section p-4 pt-5' >
         <Header/>
-        <div className=" container mt-3 pt-5 pb-4">
-          <AboutMe/>
-          <Projects/>
-          <Footer/>
-        </div>
+
+        {/*FIXME add these links to the header or create a secondary header */}
+          {/* <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </nav> */}
+
+        <Router>
+
+
+          <Routes >
+            {/* FIXME add more pages here */}
+            <Route path="/" element={<><AboutMe/> <Projects/> <Footer/></>} />
+            {/* <Route path="/about" element={<Projects />} /> */}
+          </Routes>
+        </Router>
       </div>
+
     );
   }
 }
