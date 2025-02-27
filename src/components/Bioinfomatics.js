306296e29d0dc49e858183870bd87af8b3a52621
dunfee42 <matthew.dunfee42@gmail.com>
2025-02-27 17:36:05 -0600

    changed the nav again. Added components SDLC OOP Gamedev

--- src/components/Bioinfomatics.js
+++ src/components/Bioinfomatics.js
@@ -9,7 +9,7 @@ class Bioinfomatics extends Component {
             <div className='card-container'>
             <Card className='flex-wrap card card-primary text-center'>
               
-              <h4 className=''>Bioinfomatics</h4>
+              <h1 className=''>Bioinfomatics</h1>
               <h4 className='quote'>To understand life, we must first understnad the code that writes it. - Richard Feynman</h4>
               <hr/>
               <p>
