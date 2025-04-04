59cb973263ff70d8dd9b906d97bc64865bf55934
m1dunfee <matthewgoldshire@gmail.com>
2025-04-03 21:17:32 -0500

    reworked rant component to further reduce duplicate code and updated two of the data files. This allowed for injection of the object array.

--- src/components/prospectData.js
+++ src/components/prospectData.js
@@ -1,61 +1,91 @@
-import ReactLogo from '../img/reactLogo.png';
-import Nucamp from '../img/Nucamp.png';
 import JavaLogo from '../img/javaLogo.webp';
-import Owasp from '../img/OWASP.png';
-import CppLogo from '../img/cpp_logo.png';
-import CisaLogo from '../img/CISA_Logo.png';
-import PythonLogo from '../img/pythonLogo.png'
 
 const prospectData = [
   {
-    img: [JavaLogo, CisaLogo, Owasp],
-    title: 'Security Audit',
-    category: 'Java',
-    details: 'OWASP Security Audit and CISA analysis',
-    url: 'https://github.com/m1dunfee/JavaSecurityAudit',
+    img: JavaLogo,
+    title: 'Artificial Intelligence',
+    paragraph: [
+      'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
+
+      'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
+
+      'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
+
+      'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
+
+      'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
+
+      'sub topic of interest: - LLMs linguistic relativity'
+    ]
   },
   {
-    img: [JavaLogo],
-    title: 'LetsEncrypt',
-    category: 'Java',
-    details: 'Certificate Authorities and Encryption',
-    url: 'https://github.com/m1dunfee/LetsEncrypt',
-  },
-  {
-    img: [CppLogo],
-    title: 'RSA Encryption',
-    category: 'C++',
-    details: 'The math behind RSA',
-    url: 'https://github.com/m1dunfee/RSA-Encryption',
-  },
-  {
-    img: [PythonLogo, Nucamp],
-    title: 'Prismatica',
-    category: 'Python',
-    details: 'LLM / IoT project.',
-    url: 'https://github.com/m1dunfee/Prismatica',
-  },
-  {
-    img: [ReactLogo, Nucamp],
-    title: 'Nucamp',
-    category: 'React',
-    details: 'Demo site used for teaching React.',
-    url: 'https://nucamp-react-demo-site.herokuapp.com/',
-  },
-  {
-    img: [ReactLogo],
-    title: 'Multiversal',
-    category: 'React',
-    details: 'Just a fun landing page to a discord server',
-    url: 'https://github.com/m1dunfee/Multiversal-space',
-  },
-  {
-    img: [ReactLogo],
-    title: 'Originally Yours Upholstery',
-    category: 'React',
-    details: 'Business Site',
-    url: 'https://github.com/m1dunfee/originallyyoursupholstery',
-  },
+  img: JavaLogo,
+  title: 'Artificial Intelligence',
+  paragraph: [
+    'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
+
+    'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
+
+    'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
+
+    'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
+
+    'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
+
+    'sub topic of interest: - LLMs linguistic relativity'
+  ]
+},   
+{
+  img: JavaLogo,
+  title: 'Artificial Intelligence',
+  paragraph: [
+  'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
+
+  'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
+
+  'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
+
+  'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
+
+  'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
+
+  'sub topic of interest: - LLMs linguistic relativity'
+]
+},
+{
+  img: JavaLogo,
+  title: 'Artificial Intelligence',
+  paragraph: [
+  'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
+
+  'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
+
+  'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
+
+  'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
+
+  'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
+
+  'sub topic of interest: - LLMs linguistic relativity'
+]
+},
+{
+  img: JavaLogo,
+  title: 'Artificial Intelligence',
+  paragraph: [
+  'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
+
+  'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
+
+  'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
+
+  'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
+
+  'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
+
+  'sub topic of interest: - LLMs linguistic relativity'
+]
+}
 ];
 
 export default prospectData;
