698e432aa9b92f71320c5ed8a666dc457d9d3b1b
dunfee42 <matthew.dunfee42@gmail.com>
2025-04-16 13:58:33 -0500

    topicData is done for now

--- src/components/topicData.js
+++ src/components/topicData.js
@@ -40,23 +40,6 @@ const projectData = [
 
     "At its core, Linear Algebra is about transformation. Row operations, matrix multiplication, and change of basis let us reshape a system without altering its essence. This is more than just solving equations—it’s about discovering hidden symmetries, reducing complexity, and preparing data for analysis. For example, transforming a dataset into orthogonal components through Principal Component Analysis (PCA) allows us to extract its most meaningful features while discarding noise. In this way, Linear Algebra becomes not just a tool for problem solving, but a method for understanding the shape of data and the reality it reflects."
 ]
-},
-{
-  img: JavaLogo,
-  title: 'Artificial Intelligence',
-  paragraph: [
-  'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
-
-  'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
-
-  'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
-
-  'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
-
-  'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
-
-  'sub topic of interest: - LLMs linguistic relativity'
-]
 }
 ];
 
