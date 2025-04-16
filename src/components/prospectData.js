ba2fd630b145bf4ec24a28157d13fca913c3865d
dunfee42 <matthew.dunfee42@gmail.com>
2025-04-16 15:09:17 -0500

    prospects is done

--- src/components/prospectData.js
+++ src/components/prospectData.js
@@ -1,89 +1,50 @@
 import JavaLogo from '../img/javaLogo.webp';
 
+//1. Spark → 2. Vision → 3. Connection
+
+
 const prospectData = [
   {
     img: JavaLogo,
     title: 'Artificial Intelligence',
     paragraph: [
-      'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
+      "I love Star Trek. Its premise reflects my deepest hopes for humanity—growth through exploration, cooperation, and technology. One of my favorite characters is Commander Data. His journey to understand reality and what it means to be human resonates deeply with me. As a scientist, I take comfort in facts and logic, and I see beauty in systems that model the world through reason.",
 
-      'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
-
-      'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
-
-      'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
-
-      'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
-
-      'sub topic of interest: - LLMs linguistic relativity'
+      "I believe artificial intelligence has the potential to assist humanity in profound ways—especially when grounded in objectivity and statistical insight. I believe in building AI that supports human goals with increasing autonomy and insight, guided by ethical principles and practical design."
     ]
   },
   {
   img: JavaLogo,
-  title: 'Artificial Intelligence',
+  title: 'Bioinfomatics',
   paragraph: [
-    'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
+    "Biology was my first love in science. As a Boy Scout, I was introduced to ecology and the responsibility we have to preserve and protect life. That early fascination with ecosystems evolved into a deeper interest in genetics as I got older. Superheroes always seemed too far-fetched—but the idea of rewriting the building blocks of life to solve real problems felt within reach. At the same time, I began noticing how unequally biology plays out—how some people are born into disease, disorder, or disadvantage they never chose.",
 
-    'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
-
-    'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
-
-    'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
-
-    'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
-
-    'sub topic of interest: - LLMs linguistic relativity'
+    "That sense of imbalance is what drives my interest in CRISPR and genetic engineering. I want to work on technologies that level the playing field—whether that means reducing genetic predispositions to disease or using native plants to detoxify environments damaged by industry. From editing a gene to help a child avoid a rare illness to modifying plants that absorb nickel from contaminated mining zones, I’m inspired by the possibility of recoding life for good.",
+    
+    "My foundation in computer science gives me the tools to model biological systems, while my passion for fairness and sustainability gives me the reason. Bioinformatics and gene editing are where I see those values meet—and where I hope to make my mark."
   ]
 },   
 {
   img: JavaLogo,
-  title: 'Artificial Intelligence',
+  title: 'Cyber Security',
   paragraph: [
-  'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
+    "Cybersecurity doesn’t call to me in quite the same way as other areas of computer science—but I’ve come to realize I have a natural knack for it. Concepts like SQL injections, least privilege, and secure communication protocols tend to stick with me without effort. I don’t go out of my way to memorize them—they just stay top of mind.",
 
-  'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
-
-  'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
-
-  'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
-
-  'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
-
-  'sub topic of interest: - LLMs linguistic relativity'
+    "Maybe it’s because I’ve always enjoyed daydreaming about how systems can be broken—especially in games. That same mindset naturally extends to questions like: How could I scan ports on a network? Could a buffer overflow in an isolated system reveal something unexpected? I’ve explored tools like Wireshark out of sheer curiosity, and found myself fascinated by the depth of visibility they offer into network behavior.",
+    
+    "Organizations like OWASP and CISA spark further interest by documenting known exploits, which give me scenarios to reverse-engineer or mentally model. Even if I didn’t plan to go into cybersecurity at first, it’s a space where my instincts and habits line up well with real-world needs—and I wouldn’t be surprised if I end up there."
 ]
 },
 {
   img: JavaLogo,
-  title: 'Artificial Intelligence',
+  title: 'Game Dev',
   paragraph: [
-  'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
+    "I’ve loved games ever since I first picked up Morrowind—I must’ve been around eight. Over the years, gaming became more than a pastime; it became a way of seeing systems, logic, and interaction unfold in real time. As I began learning web development, I started craving something deeper: full system design, dynamic interactions, and object behaviors that go beyond static interfaces.",
 
-  'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
-
-  'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
-
-  'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
-
-  'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
-
-  'sub topic of interest: - LLMs linguistic relativity'
-]
-},
-{
-  img: JavaLogo,
-  title: 'Artificial Intelligence',
-  paragraph: [
-  'The classical philosopher Plato was skeptical of overspecialization in education. He feared students’ wisdom would suffer as the education system failed to span the elucidation space of knowledge. If you spend all your time in one area, how can you navigate and correlate that information across different disciplines?',
-
-  'This is a valid point—omniscience impossible for any human. Knowledge is too vast and time too finite. However, we do not need to know every curve of King Hieron II crown to find out how much gold is in it. We could spend our entire lives trying to rebuild the ‘wheels’ that turn society just to understand our current position.',
-
-  'Abstraction is the key to navigating complex systems. Today we face an ever-accelerating pace of life—from societal obligations to rapid technological advancements, and even the eventual heat death of the universe. We don’t have the luxury time to reinvent the wheel.',
-
-  'Sir Isaac Newton was right when he said, “we stand on the shoulders of giants.” By abstracting the work of those who came before us, we can scale our productivity to overcome the distortions of time’s entropy and adapt to changing circumstances.',
-
-  'On that note, LLMs and ML are important tools in the school of abstraction. They use mathematical principles to uncover relationships in data and detect common patterns. A trait often deemed utterly human ‘pattern seeking behavior’ so it’s only natural that we use AI to replicate and extend our own capabilities.',
-
-  'sub topic of interest: - LLMs linguistic relativity'
+    "One of my favorite subjects in college was physics. Even though it was challenging, I found the logic behind motion, collisions, and forces fascinating—and I can’t help but imagine how powerful it would be to program that logic into a virtual world. I believe games are reflections of our own reality, just shaped through the lens of rules and imagination. It’s one thing to play them—but building them feels like an act of learning, translating real-world thought into simulated systems.",
+    
+    "I’ve also worked in game testing, and I never tired of it. I’d spend all day testing, then come home and voluntarily boot up the same game. I guess I’m a creature of habit—but now, I’d like to start designing entirely new creatures of my own."
+    
 ]
 }
 ];
