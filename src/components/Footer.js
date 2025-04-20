9aafef9e87ad83203451ef274eda6196f07d3391
m1dunfee <matthewgoldshire@gmail.com>
2025-04-20 13:11:04 -0500

    changed the elements ordering in footer to stack better in mobile view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faPhoneSquareAlt, faMapMarkerAlt, faFilePdf} from '@fortawesome/free-solid-svg-icons'
-import { Button, Card, Col, Container, Row} from 'reactstrap';
+import { Button, Col, Container, Row} from 'reactstrap';
 
 class Footer extends Component {
   render() {
@@ -11,30 +11,30 @@ class Footer extends Component {
           <Col sm>
             <FontAwesomeIcon icon={faFilePdf} className='fa-2x'/>
             <h4>Resume?</h4>
-            <hr />
             <Button outline color="primary" href="/Resume.pdf" target="_blank" rel="noopener noreferrer">
               Download PDF Resume
             </Button>
+            <hr />
           </Col>
           <Col sm>
             <FontAwesomeIcon icon={faPhoneSquareAlt} className='fa-2x'/>
             <h4>Phone</h4>
+            <Button outline color="primary" href="tel:763-393-7899">(763)-393-7899</Button>
             <hr />
-            <a href="tel:763-393-7899">(763)-393-7899</a>
           </Col>
           <Col sm>
             <FontAwesomeIcon icon={faEnvelope} className='fa-2x'/>
             <h4>Email</h4>
+            <Button outline color="primary" href="mailto:Matthew.dunfee42@gmail.com">matthew.dunfee42@gmail.com</Button>
             <hr />
-            <a href="mailto:Matthew.dunfee42@gmail.com">matthew.dunfee42@gmail.com</a>
           </Col>
           <Col sm>
             <FontAwesomeIcon icon={faMapMarkerAlt} className='fa-2x'/>
             <h4>Location</h4>
-            <hr />
-            <a href="https://www.google.com/maps/place/Burnsville,+MN+55306" target="_blank" rel="noopener noreferrer">
+            <Button outline color="primary" href="https://www.google.com/maps/place/Burnsville,+MN+55306" target="_blank" rel="noopener noreferrer">
               Burnsville, MN
-            </a>
+            </Button>
+            <hr />
           </Col>
         </Row>
       </Container>
