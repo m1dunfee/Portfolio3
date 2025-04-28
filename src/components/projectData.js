231ebe5d37ee8934d7bcbe170262f7b2454d72ee
dunfee42 <matthew.dunfee42@gmail.com>
2025-04-28 13:17:31 -0500

    added new objects to my project data and a image to public to support the new projects that will be displayed by this addion.

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -5,8 +5,37 @@ const Owasp = '/OWASP.png';
 const CppLogo = '/cpp_logo.png';
 const CisaLogo = '/CISA_Logo.png';
 const PythonLogo = '/pythonLogo.png'
+const MatLab = '/matlab.png'
 
 const projectData = [
+  {
+    img: [ReactLogo],
+    title: 'Portfolio',
+    category: 'JavaScript',
+    details: 'You\'re here!',
+    url: 'https://github.com/m1dunfee/Portfolio3',
+  },  
+  {
+    img: [CppLogo],
+    title: 'Big\'O',
+    category: 'C++',
+    details: 'This is the final project with reflections. Preliminary projects on my github labeled \"cs300\" + \'dataStructure\'',
+    url: 'https://github.com/m1dunfee/cs300Final',
+  },  
+  {
+    img: [JavaLogo],
+    title: 'jUnit',
+    category: 'Java',
+    details: 'Example of using a testing libraries to anotate and build custom test cases.',
+    url: 'https://github.com/m1dunfee/cs320jUnit',
+  },  
+  {
+    img: [MatLab],
+    title: 'Image Compression',
+    category: 'MatLab',
+    details: 'Using linear algebra to compress the pixel matrix by projecting onto pivot points, thereby reducing the matrix\'s dimensionality without compromising image clarity.',
+    url: 'https://github.com/m1dunfee/ImageCompression',
+  },
   {
     img: [JavaLogo, CisaLogo, Owasp],
     title: 'Security Audit',
