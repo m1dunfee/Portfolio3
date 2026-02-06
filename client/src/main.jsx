578db6931804222fec0999d772d3cabb6db7e937
m1dunfee <matthewgoldshire@gmail.com>
2026-02-06 09:29:36 -0600

    styling updates including bug ui and marketing colors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { BrowserRouter} from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import ScrollToTop from './hooks/ScrollToTop.jsx';
-import './index.css'
 import './index.scss';
 import App from './App.jsx'
 
